07f1563a268431a7494e98c044972043c076655f
JackHyde <jack.hyde@school.barren.kyschools.us>
2023-04-26 08:20:09 -0500

    Initial Commit

---
+++ Lists/ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsApp.swift
+//  Lists
+//
+//  Created by Jack Hyde on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
